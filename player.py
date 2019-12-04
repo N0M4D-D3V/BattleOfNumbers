9e7a5d546213741c1f17b4ac865e3a6b87ac750a
Knight <victor.developsggmail.com>
2019-12-04 20:03:50 +0100

    04/12/2019 - 20:03

--- player.py
+++ player.py
@@ -1,24 +1,28 @@
 import random
 
-class Player(object):
+class Player:
+	numbers = random.sample(range(10),5)
 
 	def __init__(self):
-		global numbers
-		for x in range(5):
-			numbers.append(random.randint(1,10))
-			print(numbers)	
-
+		print(self.numbers)
+		
 	@classmethod
 	def tryShot(self, number):
-		if number in numbers:
-			print("Golpeado")
+		if number in self.numbers:
+			self.numbers[self.numbers.index(number)] = -1
+			return True
+		else:
+			return False
 
 	@classmethod
 	def isAlive(self):
 		cont = 0
-		for x in numbers:
-			if numbers[x]==-1:
+		for x in range(5):
+			if self.numbers[x]==-1:
 				cont+=1
-		print(cont)
+		return cont!=5
 
 Prueba = Player()
+print(Prueba.tryShot(5))
+print(Prueba.isAlive())
+print(Prueba.numbers)

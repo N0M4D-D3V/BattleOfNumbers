a61a7d3f6f33bba540407e81f16d6abecabc38b3
Knight <victor.developsggmail.com>
2019-12-09 01:39:58 +0100

    09/12/2019 - 1:40

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def mainMenu():
 		userOption = int(input("Choose an option: "))
 		if(userOption == 1):
 			print("One player mode running ...")
+			player_one = Player()
+			player_two = PlayerAI()
+			gameThread(player_one, player_two)
 		elif(userOption == 2):
 			print("Two players mode running ...")
 			player_one = Player()
@@ -30,24 +33,53 @@ def gameThread(player_one, player_two):
 	players_list = [player_one,player_two]
 	turn = 0
 	while(running):
+		print(">< Player",turn,"><")
 		print(player_one.numbers)
 		print(player_two.numbers)
-		
-		players_list[nextTurn(turn)].tryShot(int(input("SHOT! ")))
+		players_list[nextTurn(turn)].tryShot(players_list[turn].shot())
 		if(players_list[nextTurn(turn)].isAlive() == False):
 			running = False
-		turn = nextTurn(turn)
+			print("Player",turn,"wins!")
+		else:
+			turn = nextTurn(turn)
 
 def nextTurn(turn):
 	return (turn+1)%2
+
+def generateRandomList():
+	numbers = random.sample(range(10),5)
+	return numbers
 		
 #----------------------- PLAYER CLASS ---------------------------------------------------
-class Player:
+class Player(object):
 	numbers = random.sample(range(10),5)
-
 	def __init__(self):
-		print(self.numbers)
-		
+		print("Human Player created ...")
+        
+	@classmethod
+	def tryShot(self, number):
+		if number in self.numbers:
+			self.numbers[self.numbers.index(number)] = -1
+
+	@classmethod
+	def isAlive(self):
+		cont = 0
+		for x in range(5):
+			if self.numbers[x]==-1:
+				cont+=1
+		return cont!=5
+
+	@classmethod
+	def shot(self):
+        	return int(input("SHOT! -> "))
+    
+#----------------------------------------------------------------------------------------
+#------------------------ PLAYER-AI CLASS ------------------------------------------------
+class PlayerAI(object):
+	numbers = random.sample(range(10),5)
+	def __init__(self):
+		print("AI Player created ...")
+	
 	@classmethod
 	def tryShot(self, number):
 		if number in self.numbers:
@@ -63,5 +95,9 @@ class Player:
 			if self.numbers[x]==-1:
 				cont+=1
 		return cont!=5
+
+	@classmethod
+	def shot(self):
+        	return random.randint(0,11)
 #----------------------------------------------------------------------------------------
 mainMenu()

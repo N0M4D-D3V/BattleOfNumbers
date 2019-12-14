6297a9522bdfa80b35213bc87da3e0b504710303
Knight <victor.developsggmail.com>
2019-12-14 19:00:39 +0100

    14/12/2019 - 19:00

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import random
 
 def mainMenu():
 	running = True
+	player_one=Player()
+	player_two=Player()
+	player_ai=PlayerAI()
+
 	while(running):
 		print("")
 		print("<>---<>---<> BATTLE OF NUMBERS <>---<>---<>")
@@ -13,13 +17,13 @@ def mainMenu():
 		userOption = int(input("Choose an option: "))
 		if(userOption == 1):
 			print("One player mode running ...")
-			player_one = Player(generateRandomList())
-			player_two = PlayerAI(generateRandomList())
-			gameThread(player_one, player_two)
+			player_one.reload()
+			player_ai.reload()
+			gameThread(player_one, player_ai)
 		elif(userOption == 2):
 			print("Two players mode running ...")
-			player_one = Player(generateRandomList())
-			player_two = Player(generateRandomList())
+			player_one.reload()
+			player_two.reload()
 			gameThread(player_one, player_two)
 		elif(userOption == 3):
 			print("Good bye bitch !")
@@ -32,7 +36,7 @@ def gameThread(player_one, player_two):
 	players_list = [player_one,player_two]
 	turn = 0
 	while(running):
-		print(">< Player",turn,"><")
+		print(">< Player",turn,"><",end="\n\r\n\r")
 		print(player_one)
 		print(player_two)
 		players_list[nextTurn(turn)].tryShot(players_list[turn].shot())
@@ -43,27 +47,29 @@ def gameThread(player_one, player_two):
 			turn = nextTurn(turn)
 def nextTurn(turn):
 	return (turn+1)%2
-
-def generateRandomList():
-	return random.sample(range(10),5)
 		
 #----------------------- PLAYER CLASS ---------------------------------------------------
 class Player(object):
-	numbers = random.sample(range(10),5)
-	def __init__(self, randomSample):
-		self.numbers = randomSample
+	numbers = []
+	def __init__(self):
 		print("Human Player created ...")
 	@classmethod
 	def tryShot(self, number):
 		if number in self.numbers:
 			self.numbers[self.numbers.index(number)] = -1
-			print("... HITTED!")
+			print("\n\r... HITTED!\n\r")
 	@classmethod
 	def isAlive(self):
 		return self.numbers.count(-1)!=5
 	@classmethod
 	def shot(self):
-        	return int(input("SHOT! -> "))
+		try:
+			shot = int(input("SHOT! -> "))
+		except ValueError:
+			print("ValueError D:! ")
+			shot = 0
+		finally:
+			return shot
 	@classmethod
 	def __str__(self):
 		ocult_values = "["
@@ -74,24 +80,30 @@ class Player(object):
 				ocult_values = ocult_values + " ?"
 		ocult_values = ocult_values + " ]"
 		return ocult_values
+	@classmethod
+	def reload(self):
+		self.numbers = random.sample(range(10),5)
 #----------------------------------------------------------------------------------------
 #------------------------ PLAYER-AI CLASS ------------------------------------------------
 class PlayerAI(object):
-	numbers = random.sample(range(10),5)
-	def __init__(self, randomSample):
-		self.numbers = randomSample
+	numbers = []
+	numbersShoted = []
+	def __init__(self):
 		print("AI Player created ...")
 	@classmethod
 	def tryShot(self, number):
 		if number in self.numbers:
 			self.numbers[self.numbers.index(number)] = -1
-			print("... HITTED!")
+			print("\n\r... HITTED!\n\r")
 	@classmethod
 	def isAlive(self):
 		return self.numbers.count(-1)!=5
 	@classmethod
 	def shot(self):
 		shot = random.randint(0,11)
+		while(shot in self.numbersShoted):
+			shot = random.randint(0,11)
+		self.numbersShoted.append(shot)
 		print("SHOT! -> ",end="")
 		print(shot)
 		return shot
@@ -105,5 +117,9 @@ class PlayerAI(object):
 				ocult_values = ocult_values + " ?"
 		ocult_values = ocult_values + " ]"
 		return ocult_values
+	@classmethod
+	def reload(self):
+		self.numbers = random.sample(range(10),5)
+		numbersShoted = []
 #----------------------------------------------------------------------------------------
 mainMenu()
